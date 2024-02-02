472036d3a4aff180d9e4c11ed9b875d692c63c24
Ryan Twohey <rjtwoh01@gmail.com>
2024-02-01 21:26:21 -0500

    started a basic demo scene where the player moves via point to click

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 targetPosition;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.z = 0f;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
+    }
+}
