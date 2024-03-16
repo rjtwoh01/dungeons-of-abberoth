05a82df3f10755f835e1344a2338e9e9ce7ec31d
Ryan Twohey <rjtwoh01@gmail.com>
2024-03-16 09:34:07 -0400

    enemies can now chase players

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public GameObject target;
+    public float speed = 2f;
+    [SerializeField]
+    private float range = 0.75f;
+    private float currentDistance;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentDistance = Vector2.Distance(transform.position, target.transform.position);
+
+        if (currentDistance > range)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        }
     }
 }
